94714cd4e702c7d0af903631341b21cc29c23a33
abhl05 <abhijnanpodder@gmail.com>
2026-01-08 04:45:26 +0600

    Implement Dijkstra's algorithm in problem1.cpp and Bellman-Ford algorithm in problem2.cpp for shortest path and currency arbitrage detection respectively.

---
+++ DSA2/Offline_2/problem1.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+typedef long long ll;
+#define vll vector<ll>
+#define vllp vector<pair<ll, ll>>
+#define endl '\n'
+#define fastIO() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define vin(x) for (auto &i : x) cin >> i;
+#define vout(x) for (auto &i : x) cout << i << " ";
+#define all(x) x.begin(), x.end()
+#define forn(i, n) for(int i = 0; i < (n); i++)
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n, m, q;
+    cin >> n >> m >> q;
+    vector <vector<pair<int, int>>> adjw(n + 1);
+
+    forn(i, m) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adjw[u].push_back({v, w});
+        adjw[v].push_back({u, w});
+    }
+
+    // floyd warshall
+    vector <vector<int>> dist(n + 1, vector<int>(n + 1, INF));
+    
+    for(int u = 1; u <= n; u++) {
+        for(auto edge : adjw[u]) {
+            int v = edge.first;
+            int w = edge.second;
+            dist[u][v] = u != v ? w : 0;
+        }
+    }
+    for(int k = 1; k <= n; k++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    while(q--) {
+        int u, v;
+        cin >> u >> v;
+        if(dist[u][v] == INF) {
+            cout << -1 << endl;
+        } else {
+            cout << dist[u][v] << endl;
+        }
+    }
+}
+
+signed main() {
+    fastIO();
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
--- DSA2/Offline_2/problem2.cpp
+++ DSA2/Offline_2/problem2.cpp
@@ -23,7 +23,7 @@ void solve() {
         currencyIndex[currency] = i + 1;
     }
     cin >> m;
-    vector <tuple<int  >> adjw(n + 1);
+    vector <tuple<int, int, double>> edges(n + 1);
 
     forn(i, m) {
         string u, v;
@@ -31,7 +31,7 @@ void solve() {
         cin >> u >> rate >> v;
         int u_node = currencyIndex[u];
         int v_node = currencyIndex[v];
-        adjw.push_back({u_node, v_node, -log(rate)});
+        edges.push_back({u_node, v_node, -log(rate)});
         
     }
 
@@ -44,18 +44,8 @@ void solve() {
             }
         }
     }
-    for(int k = 1; k <= n; k++) {
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(dist[i][k] != INF && dist[k][j] != INF) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-                }
-            }
-        }
-    }
-
-    for(int i = 1; i <= n; i++) {
-        if(dist[i][i] < 0) {
+    for (auto [u, v, w] : edges) {
+        if (dist[v] > dist[u] + w) {
             cout << "Yes" << endl;
             return;
         }
